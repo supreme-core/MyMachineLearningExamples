6d5f29cf0febcb8522c05e0e206ef48986b60a99
Koro <eric.ct.liu.core@gmail.com>
2019-06-15 16:16:59 -0600

    setup scala env

--- build.sbt
+++ build.sbt
@@ -5,8 +5,6 @@ version := "0.1"
 
 scalaVersion := "2.12.6"
 
-scalaVersion := "2.12.6"
-
 libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.0"
 libraryDependencies += "org.apache.spark" %% "spark-mllib" % "2.4.0"
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.0"
---
+++ src/main/scala/FirstTest.scala
@@ -0,0 +1,16 @@
+import org.apache.spark.{SparkConf, SparkContext}
+
+object FirstTest {
+
+  def main(args: Array[String]) : Unit = {
+    val conf = new SparkConf()
+    conf.setMaster("local")
+    conf.setAppName("First Application")
+    val sc = new SparkContext(conf)
+
+    val rddl = sc.makeRDD(Array(1,2,3,4,5,6,7))
+    rddl.collect().foreach(println)
+
+  }
+}
+
